05e38f136d2eb819be6f795e4dc8f38bc472d1cd
FelixYifeiWang <yifei_wang2@brown.edu>
2024-10-30 21:47:13 -0400

    a

--- contact-script.php
+++ contact-script.php
@@ -1,18 +1,35 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "admin@dreamin.land";
-    $subject = "New Contact Form Submission";
-    $name = htmlspecialchars($_POST["name"]);
-    $email = htmlspecialchars($_POST["email"]);
-    $message = htmlspecialchars($_POST["message"]);
-    $headers = "From: " . $email;
+    $mail = new PHPMailer(true);
 
-    $fullMessage = "Name: $name\nEmail: $email\nMessage:\n$message";
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Or use Amazon SES SMTP if preferred
+        $mail->SMTPAuth = true;
+        $mail->Username = 'admin@dreamin.land';
+        $mail->Password = 'DreamIn2022';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
 
-    if (mail($to, $subject, $fullMessage, $headers)) {
-        echo "Thank you for your message!";
-    } else {
-        echo "There was a problem sending your message. Please try again.";
+        // Recipients
+        $mail->setFrom($_POST['email'], $_POST['name']);
+        $mail->addAddress('admin@dreamin.land'); // Your receiving email address
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body    = 'Name: ' . $_POST['name'] . '<br>Email: ' . $_POST['email'] . '<br>Message: ' . nl2br($_POST['message']);
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 ?>
